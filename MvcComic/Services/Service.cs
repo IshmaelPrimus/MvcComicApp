bfa1ffd911842c2c471b698bf6553dd571637689
Ishmael Primus <ishmaelprimus@yahoo.com>
2024-11-24 23:52:13 -0600

    added .gitignore

--- MvcComic/Services/Service.cs
+++ MvcComic/Services/Service.cs
@@ -1,6 +1,32 @@
-﻿namespace MvcComic.Controllers
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+public class ComicVineService
 {
-    public class Class
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey;
+
+    public ComicVineService(HttpClient httpClient, string apiKey)
     {
+        _httpClient = httpClient;
+        _apiKey = "2194908e26505271c0a8b22937d61d9af0d9ac54";
+    }
+
+    public async Task<string?> GetIssueImageAsync(string issueName, int issueNumber)
+    {
+        var url = $"https://comicvine.gamespot.com/api/search/?api_key={_apiKey}&format=json&sort=name:asc&resources=issue&query=\"{issueName}\"&filter=name:{issueName},issue_number:{issueNumber}";
+        var response = await _httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var json = JObject.Parse(content);
+            var results = json["results"]?.FirstOrDefault();
+            return results?["image"]?["original_url"]?.ToString();
+        }
+
+        return null;
     }
 }
+
