7940394dbf23ec115b96544c8a47bcb1e6eb9693
Ishmael Primus <ishmaelprimus@yahoo.com>
2024-11-28 14:02:44 -0600

    fixed https issue

--- MvcComic/Migrations/20241128200013_AddImageUrlToComic.Designer.cs
+++ MvcComic/Migrations/20241128200013_AddImageUrlToComic.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcComic.Data;
 
@@ -11,9 +12,11 @@ using MvcComic.Data;
 namespace MvcComic.Migrations
 {
     [DbContext(typeof(MvcComicContext))]
-    partial class MvcComicContextModelSnapshot : ModelSnapshot
+    [Migration("20241128200013_AddImageUrlToComic")]
+    partial class AddImageUrlToComic
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MvcComic/Migrations/20241128200013_AddImageUrlToComic.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcComic.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToComic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Comic",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Comic");
+        }
+    }
+}
